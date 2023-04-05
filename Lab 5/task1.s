b0fdeb147e0b7f47c113ebfdf26ded71903b928d
Sadmin-Tahmid <sadmid26tahmid@gmail.com>
2023-04-06 00:05:07 +0600

    task 1 complete

--- Lab 5/task1.s
+++ Lab 5/task1.s
@@ -1,52 +1,24 @@
 	    AREA task1, CODE, READONLY
 		ENTRY
 		EXPORT main
-			
-sum
-; Push return address
-	STR LR, [SP, #-4]!
 
-; Push registers (including stack frame)
-	STR IP, [SP, #-4]!
-	STR R0, [SP, #-4]!
-	STR R1, [SP, #-4]!
-
-; Reserve space for local variables
-	SUB SP, SP, #4
-
-; Get first argument from stack
-	LDR R0, [SP, #16]
-
-; Get second argument from stack
-	LDR R1, [SP, #12]
-
-; Add the two values and store in R0
-	ADD R0, R0, R1
-
-; Discard local variables
-	ADD SP, SP, #4
-
-; Pop registers
-	LDR R1, [SP], #4
-	LDR R0, [SP], #4
-	LDR IP, [SP], #4
-
-; Pop return address and return
-	LDR LR, [SP], #4
-	BX LR
+sum PROC
+	POP {r2, r3}
+ 
+	ADD r0,r2,r3
+	PUSH {r0}
+	BX lr
+	ENDP
 
 main
-; Push arguments onto stack
-	MOV R0, #5
-	MOV R1, #7
-	STR R1, [SP, #-4]!
-	STR R0, [SP, #-4]!
-
-; Call add_two_values
-	BL sum
-
-; Get result from R0
-	MOV R0, R0
-	
-Stop  B Stop
-     END
+    MOV r1, #6 
+	MOV r0, #7
+    PUSH {lr} ; Save the return address
+    PUSH {r0, r1} ; Push arguments onto the stack
+    BL sum ; Call the function
+	POP {r5} ; pop return value
+    POP {pc} ; Pop the return address into pc   
+    ; Halt the program
+    B Stop
+Stop B Stop ; Infinite loop to halt the program
+ END		 
