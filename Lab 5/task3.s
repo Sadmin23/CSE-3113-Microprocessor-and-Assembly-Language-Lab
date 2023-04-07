0ad8ecbd3bd094d3bf42ec1334dfa245b935746d
Sadmin-Tahmid <sadmid26tahmid@gmail.com>
2023-04-07 20:14:06 +0600

    task 3 complete

--- Lab 5/task3.s
+++ Lab 5/task3.s
@@ -7,33 +7,32 @@ n DCD 5
 
 min PROC
 	POP {r2, r3}
-	CMP r3, #1
+	CMP r3, #0
 	BEQ base
 	
 	SUB r3, r3, #1
-	LDR r5, [r2, r3, LSL #2]
-    PUSH {r5}
-    PUSH {r2, r3}
-	BL min
-	POP {r6, r7}
-	CMP r6, r7
-	BGE push_r7
-	PUSH {r6}
-	BX lr
+	LDR r4, [r2, r3, LSL #2]
+	CMP r5, r4
+	BGE push_r5
+	PUSH {r2, r3}
+	B min
+	ENDP
 
-push_r7
-	PUSH {r7}
-	BX lr
+push_r5
+	MOV r5, r4
+	PUSH {r2, r3}
+	B min
+	ENDP
 
 base
-	LDR r4, [r2]
-	PUSH {r4}
+	PUSH {r5}
 	BX lr
 
 main
 	LDR r0, = numbers
 	LDR r1, = n
 	LDR r1, [r1]
+	MOV r5, #100
     PUSH {lr}
     PUSH {r0, r1}
     BL min
