91bd5f5575131e13691575697b575d2ec514ec66
Sadmin-Tahmid <sadmid26tahmid@gmail.com>
2023-04-07 12:06:23 +0600

    task 3 updated

--- Lab 5/task3.s
+++ Lab 5/task3.s
@@ -5,25 +5,40 @@
 numbers DCD 4, 7, 3, 9, 1;
 n DCD 5
 
-num PROC
+min PROC
 	POP {r2, r3}
-	MOV r1, #0         ; Load the index of the element we want to access into r1
-	LSL r1, r1, #2     ; Multiply the index by 4
-	ADD r3, r2, r1     ; Add the base address and the index to get the memory address of the element
-	LDR r3, [r3]       ; Load the value at the memory address into r3
-	PUSH {r3}
-	BX lr
+	CMP r3, #1
+	BEQ base
+	
+	SUB r3, r3, #1
+	LDR r5, [r2, r3, LSL #2]
+	CMP r4, r5
+	BGE push_r5
+	PUSH {r4, r3}
+	B min
+
+push_r5
+	PUSH {r5, r3}
+	B min	
+	
 	ENDP
+
+base
+	MOV r7, #0
+	LDR r4, [r2, r7, LSL #2]
+	PUSH {r4, r3}
+	BX lr
 
 main
 	LDR r0, = numbers
 	LDR r1, = n
-    PUSH {lr} ; Save the return address
-    PUSH {r0, r1} ; Push arguments onto the stack
-    BL num ; Call the function
-	POP {r5} ; pop return value
-    POP {pc} ; Pop the return address into pc   
-    ; Halt the program
+	LDR r1, [r1]
+    PUSH {lr}
+    PUSH {r0, r1}
+    BL min
+	POP {r6}
+    POP {pc}
+	
     B Stop
-Stop B Stop ; Infinite loop to halt the program
+Stop B Stop
  END		 
