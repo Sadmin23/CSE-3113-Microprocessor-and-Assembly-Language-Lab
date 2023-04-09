cb182c75f2c7eab27c82920cd6a77c9cd09055ca
Sadmin-Tahmid <sadmid26tahmid@gmail.com>
2023-04-09 19:26:13 +0600

    task 3 complete

--- Lab 5/task3.s
+++ Lab 5/task3.s
@@ -7,21 +7,19 @@ y DCD 7
 
 sum PROC
 	POP {r2, r3}
-	LDR r2, [r2]
-	LDR r3, [r3]
-	ADD r4,r2,r3
-	PUSH {r4}
+	LDR r4, [r2]
+	LDR r5, [r3]
+	ADD r6,r4,r5
+	PUSH {r6}
 	BX lr
 	ENDP
 
 main
 	LDR r0, = x
 	LDR r1, = y
-	PUSH {lr}
     PUSH {r0, r1}
     BL sum
-	POP {r5}
-    POP {pc}
+	POP {r7}
 	
     B Stop
 Stop B Stop
