d60673619749b807fd0a552c86ea80495feff8a4
Sadmin-Tahmid <sadmid26tahmid@gmail.com>
2023-04-09 12:36:38 +0600

    task 4 completed

--- Lab 5/task4.s
+++ Lab 5/task4.s
@@ -5,7 +5,7 @@
 numbers DCD 4, 7, 3, 9, 8;
 n DCD 5
 
-min PROC
+min
 	POP {r2, r3}
 	CMP r3, #0
 	BEQ base
@@ -16,28 +16,22 @@ min PROC
 	BGE push_r5
 	PUSH {r2, r3}
 	B min
-	ENDP
 
 push_r5
 	MOV r5, r4
 	PUSH {r2, r3}
 	B min
-	ENDP
 
 base
-	PUSH {r5}
-	BX lr
+	MOV PC, lr
 
 main
 	LDR r0, = numbers
 	LDR r1, = n
 	LDR r1, [r1]
 	MOV r5, #100
-    PUSH {lr}
     PUSH {r0, r1}
     BL min
-	POP {r6}
-    POP {pc}
 	
     B Stop
 Stop B Stop
